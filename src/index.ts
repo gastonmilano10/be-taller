fbbe93a1f59c8412ffda1b5322278aefc8d87bc2
Enzo <c.emoyan@tessa.com.uy>
2026-03-07 17:50:07 -0300

    feat: add rate limit to all routes

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,24 @@ import clientsRoutes from "./routes/clients";
 import vehiclesRoutes from "./routes/vehicles";
 import servicesRoutes from "./routes/services";
 
+const rateLimit = require("express-rate-limit");
+
 const port = process.env.PORT || 3001;
 
 dotenv.config();
 
 const app: Express = express();
+
+// SE DEFINE RATE LIMIT RUTE, PARA LIMITAR EL NUMERO DE PETICIONES QUE SE PUEDEN HACER DESDE UNA MISMA IP EN UN PERIODO DE TIEMPO DETERMINADO
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // lIMITA CADA IP A 100 PETICIONES POR CADA 15 MINUTOS
+  message: "Demasiadas peticiones, por favor intente nuevamente más tarde.", // MENSAJE DE ERROR CUANDO SE SUPERA EL LIMITE
+});
+
+// SE APLICA RATE LIMIT A TODAS LAS RUTAS
+app.use(limiter);
+
 app.use(cors());
 app.use(express.json());
 
